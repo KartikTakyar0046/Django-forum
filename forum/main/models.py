30503f01fc6012f0cbb56bd2aaa5b531f45a775e
KartikTakyar0046 <42770126+KartikTakyar0046@users.noreply.github.com>
2020-10-01 16:13:06 +0530

    configured user in models along with message

--- forum/main/models.py
+++ forum/main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Message(models.Model):
     text = models.TextField()
+    publisher=models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
